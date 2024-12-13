cb103f3ae5e88aa5b80f0d8fa3f3a92958cdac7a
Andrew Jarrett <andrew.jarrett@gatech.edu>
2024-12-13 16:58:25 -0500

    Move rayColor and render method to Camera

--- src/camera.zig
+++ src/camera.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 
 const Point3 = @import("vec.zig").Point3;
 const Vec3 = @import("vec.zig").Vec3;
+const Ray = @import("ray.zig").Ray;
+const Color = @import("color.zig").Color;
+const Color3 = @import("color.zig").Color3;
+const HittableList = @import("hittable.zig").HittableList;
+const Interval = @import("interval.zig").Interval;
+const PPM = @import("ppm.zig").PPM;
+
+const inf = std.math.inf(f64);
 
 const Image = struct {
     width: usize = 100,
@@ -91,6 +99,48 @@ pub const Camera = struct {
             .pixel0 = pixel0,
         };
     }
+
+    pub fn render(self: Camera, world: HittableList) !void {
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+
+        // Setup Image/PPM
+        var ppm = PPM.init(allocator, self.image.width, self.image.height);
+        defer ppm.deinit();
+
+        for (0..ppm.height) |j| {
+            std.log.info("\rScanlines remaining: {d} ", .{ppm.height - j});
+            for (0..ppm.width) |i| {
+                const pixelCenter = self.pixel0
+                    .add(self.du.mulScalar(@floatFromInt(i)))
+                    .add(self.dv.mulScalar(@floatFromInt(j)));
+                const rayDir = pixelCenter.sub(self.center); // We don't really need to subtract (0, 0, 0) from the pixel center
+                const ray = Ray.init(self.center, rayDir);
+                ppm.pixels[i + j * ppm.width] = Camera.rayColor(ray, world);
+            }
+        }
+        std.log.info("\rDone.\n", .{});
+
+        // Save the file
+        try ppm.saveBinary("images/chapter6.ppm");
+    }
+
+    fn rayColor(ray: Ray, world: HittableList) Color {
+        // Find the point where we hit the sphere
+        const hitRecord = world.hit(ray, Interval.init(0, inf));
+        if (hitRecord) |rec| {
+            const n: Vec3 = rec.normal.add(Color3.init(1, 1, 1)).mulScalar(0.5);
+            return Color.init(n.x(), n.y(), n.z());
+        }
+
+        const unitDir = ray.dir.unit(); // Normalize between -1 and 1
+        const a = 0.5 * (unitDir.y() + 1.0); // Shift "up" by 1 and then divide in half to make it between 0 - 1
+        // Linear interpolate: white * (1.0 - a) + blue * a -> as y changes, gradient changes from blue to white
+        const vec = Color.init(1.0, 1.0, 1.0).pixel.mulScalar(1.0 - a)
+            .add(Color.init(0.5, 0.7, 1.0).pixel.mulScalar(a));
+        return Color.init(vec.x(), vec.y(), vec.z());
+    }
 };
 
 test "Image" {
@@ -158,9 +208,6 @@ test "Camera" {
     };
     const init = Camera.init(1.0, 400, (16.0 / 9.0));
 
-    std.debug.print("camera - du: {s}, dv: {s}, pixel0: {s}\n", .{ camera.du, camera.dv, camera.pixel0 });
-    std.debug.print("init - du: {s}, dv: {s}, pixel0: {s}\n", .{ init.du, init.dv, init.pixel0 });
-
     try std.testing.expectEqual(2.0, camera.focalLength);
     try std.testing.expectEqual(800, camera.image.width);
     try std.testing.expectEqual(400, camera.image.height);
@@ -182,4 +229,29 @@ test "Camera" {
     try std.testing.expectEqual(Vec3.init(0.008888888888888889, 0, 0), init.du);
     try std.testing.expectEqual(Vec3.init(0, -0.008888888888888889, 0), init.dv);
     try std.testing.expectEqual(Vec3.init(-1.7733333333333332, 0.9955555555555555, -1), init.pixel0);
+}
+
+test "Camera render()" {
+    const Hittable = @import("hittable.zig").Hittable;
+
+    // Figure out aspect ratio, image width, and height
+    const aspectRatio = 16.0 / 9.0;
+    const camera = Camera.init(1.0, 400, aspectRatio);
+
+    // World
+    var world = HittableList.init(std.testing.allocator);
+    defer world.deinit();
+    world.add(Hittable.init(.sphere, .{ .center = Point3.init(0, 0, -1), .radius = 0.5 }));
+    world.add(Hittable.init(.sphere, .{ .center = Point3.init(0, -100.5, -1), .radius = 100 }));
+
+    // Render and save the file
+    try camera.render(world);
+
+    const expected = try std.fs.cwd().readFileAlloc(std.testing.allocator, "test-files/chapter6.ppm", 5e5);
+    defer std.testing.allocator.free(expected);
+
+    const actual = try std.fs.cwd().readFileAlloc(std.testing.allocator, "images/chapter6.ppm", 5e5);
+    defer std.testing.allocator.free(actual);
+
+    try std.testing.expectEqualStrings(expected, actual);
 }
