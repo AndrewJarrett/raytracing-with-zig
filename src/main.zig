2546a2db1dbd7685871f09c5ed7fcfe37532408b
Andrew Jarrett <andrew.jarrett@cfacorp.com>
2026-07-20 23:19:04 -0400

    chore: refactor main
    * Pass stdOption for log level from the build parameter and set the
    default log level
    * Pre-allocate the Image pixels and Scene world fields
    * Call new Camera.builder method

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,22 @@ const DefaultPrng = std.Random.DefaultPrng;
 const Io = std.Io;
 
 const config = @import("config");
+const stdOptions = @import("std_options");
 
 const Camera = @import("camera.zig").Camera;
 const Image = @import("Image.zig");
+const Color = @import("color.zig").Color;
 const Point3 = @import("vec.zig").Point3;
 const Scene = @import("Scene.zig");
+const SceneType = @import("Scene.zig").SceneType;
+const Object = @import("hittable.zig").Object;
 const util = @import("util.zig");
 
 const inf = std.math.inf(f64);
+
+pub const std_options: std.Options = .{
+    .log_level = @field(std.log.Level, @tagName(stdOptions.log_level)),
+};
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
@@ -20,19 +28,25 @@ pub fn main(init: std.process.Init) !void {
 
     // Use or generate a common seed
     const seed = initSeed(io);
-    std.log.info("Using seed: 0x{x}", .{ seed });
 
     // Create threaded Io
     var threaded = std.Io.Threaded.init(gpa, .{});
     defer threaded.deinit();
 
+    const world = try alloc.alloc(Object, @intFromEnum(SceneType.chapter14));
+    defer alloc.free(world);
+    var scene = Scene{ .seed = seed, .world = world };
+    scene.generateScene(.chapter14);
+
     // Create Image
-    const image = Image.init(alloc, io, config.imgWidth, config.aspectRatio);
-    std.log.info("Rendering image: {f}", .{ image });
+    const height = Image.getHeightFromWidthAndRatio(config.imgWidth, config.aspectRatio);
+    const size = @as(u32, config.imgWidth) * @as(u32, height);
+    const pixels = try alloc.alloc(Color, size);
+    const image = Image{ .width = config.imgWidth, .height = height, .pixels = pixels };
 
     // Camera / render
-    var camera = initCamera(alloc, threaded.io(), image, seed);
-    try camera.render();
+    var camera = initCamera(image, scene);
+    try camera.render(threaded.io());
 }
 
 inline fn initSeed(io: Io) u64 {
@@ -45,9 +59,9 @@ inline fn initSeed(io: Io) u64 {
     }
 }
 
-inline fn initCamera(alloc: Allocator, io: Io, image: Image, seed: u64) Camera {
-    // Builv Camera
-    return Camera.builder(alloc, io, image, seed)
+inline fn initCamera(image: Image, scene: Scene) Camera {
+    // Build Camera
+    return Camera.builder(image, scene)
         .setDefocusAngle(0.6)
         .setFocusDist(10)
         .setViewport(Point3{ 13, 2, 3 }, Point3{ 0, 0, 0 }, 20)
@@ -70,16 +84,23 @@ test "main" {
     const io = std.testing.io;
     const gpa = std.testing.allocator;
 
+    const world = try gpa.alloc(Object, @intFromEnum(SceneType.chapter14));
+    defer gpa.free(world);
+
     // Use or generate a common seed
     const seed = initSeed(io);
+    var scene = Scene{ .seed = seed, .world = world };
+    scene.generateScene(.chapter14);
 
-    var image = Image.init(gpa, io, config.imgWidth, config.aspectRatio);
-    defer image.deinit();
+    const height = Image.getHeightFromWidthAndRatio(config.imgWidth, config.aspectRatio);
+    const size = @as(u32, config.imgWidth) * @as(u32, height);
+    const pixels = try gpa.alloc(Color, size);
+    defer gpa.free(pixels);
+    const image = Image{ .width = config.imgWidth, .height = height, .pixels = pixels };
 
     // Camera
-    var camera = initCamera(gpa, io, image, seed);
-    try camera.render();
-    defer camera.deinit();
+    var camera = initCamera(image, scene);
+    try camera.render(io);
 
     const expected = try std.Io.Dir.cwd().readFileAlloc(io, "test-files/" ++ config.fileName, gpa, Io.Limit.limited(5e5));
     defer gpa.free(expected);
